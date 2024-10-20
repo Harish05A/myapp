91e4a6e5396d42e62b3095ab596836f127cfd708
Harish05A <harisharumugam2005@gmail.com>
2024-10-20 08:41:39 +0000

    Homepage created

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class homePage extends StatelessWidget {
+  const homePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("My Chat App"),
+      ),
+      body: Container(
+        child: Image.network("https://picsum.photos/250?image=9", height: 10,),
+      ),
+    );
+  }
+}
