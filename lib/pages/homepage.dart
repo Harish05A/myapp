fd0345ef2863d9de067a3a35c9287f2c60cc1745
Harish05A <harisharumugam2005@gmail.com>
2024-10-20 14:41:22 +0000

    Homepage template created

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -6,12 +6,65 @@ class homePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("My Chat App"),
+        title: Text("Request Hub"),
+        backgroundColor: Colors.amber,
       ),
-      body: Container(
-        child: Image.network("https://picsum.photos/250?image=9", height: 1000,),
+      body: ListView(
+        children: [
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  Text(
+                    "Good Morning !!",
+                  ),
+                  Text("Mr. XYZ"),
+                ],
+              ),
+            ),
+          ),
+          Image.network(
+              "https://content3.jdmagicbox.com/comp/kanchipuram/n1/9999p4112.4112.091020145603.q8n1/catalogue/rajalakshmi-engineering-college-thandalam-kanchipuram-engineering-colleges-gvbvtv27al.jpg"),
+          Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(color: Colors.black, height: 100, width: 100),
+                      Container(color: Colors.blue, height: 100, width: 100),
+                      Container(color: Colors.amber, height: 100, width: 100),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(color: Colors.black, height: 100, width: 100),
+                      Container(color: Colors.blue, height: 100, width: 100),
+                      Container(color: Colors.amber, height: 100, width: 100),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home,color: Colors.black,), label: "Home",),
+          BottomNavigationBarItem(icon: Icon(Icons.search,color: Colors.black), label: "Search",),
+          BottomNavigationBarItem(icon: Icon(Icons.mail,color: Colors.black), label: "Messages"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings,color: Colors.black), label: "Profile"),
+        ],
       ),
     );
   }
